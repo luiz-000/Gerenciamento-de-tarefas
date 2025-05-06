76f71cf8d46e589d197ef007bfa001ab226666f0
e <luizeduardoferreirasouza@gmail.com>
2025-05-06 19:47:08 -0300

    Adicionando componente de titulo das paginas

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import style from "./Title.module.css";
+
+const Title = () => {
+    return (
+        <div className={style.container}>
+            <h1> GERENCIAMENTO DE TAREFAS </h1>
+        </div>
+    );
+};
+
+export default Title;
