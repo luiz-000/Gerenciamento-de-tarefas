4cdf4b0085dc64e856ad5e3cad841b53946167d5
e <luizeduardoferreirasouza@gmail.com>
2025-05-06 19:47:56 -0300

    Atualizando a page Home onde os componentes são importados

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import style from "./Home.module.css";
 
+import Title from "../../components/Title/Title";
+import Inputs from "../../components/Inputs/Inputs";
+import Tarefas from "../../components/Tarefas/Tarefas";
+
 const Home = () => {
+    const [tarefas, setTarefas] = useState([]);
+
+    function salvarTarefa(titulo, descricao) {
+        const novaTarefa = { titulo: titulo, descricao: descricao };
+
+        if (novaTarefa.titulo === "" || novaTarefa.descricao === "") {
+            return alert("Preencha os campos");
+        }
+        setTarefas([...tarefas, novaTarefa]);
+    }
+
     return (
         <div className={style.container}>
-            <h1> HOME </h1>
+            <Title />
+            <Inputs salvarTarefa={salvarTarefa} />
+            <Tarefas tarefas={tarefas} />
         </div>
     );
 };
