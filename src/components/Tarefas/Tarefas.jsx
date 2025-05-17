ae7d93618eb2b718606e8cc59ec3a762ff36dd73
luiz-000 <luizeduardoferreirasouza@gmail.com>
2025-05-17 18:50:47 -0300

    Inserindo animação no componente

--- src/components/Tarefas/Tarefas.jsx
+++ src/components/Tarefas/Tarefas.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+import ClearIcon from "@mui/icons-material/Clear";
 
 import style from "./Tarefas.module.css";
 
-const Tarefas = ({ tarefas }) => {
+const Tarefas = ({ tarefas, removerTarefa }) => {
     return (
         <div className={style.container}>
             <ul>
-                {tarefas.map((tarefas, index) => (
-                    <li key={index}>
-                        {tarefas.titulo}
-                        <button> algo </button>
-                    </li>
-                ))}
+                <AnimatePresence>
+                    {tarefas.map((tarefa) => (
+                        <motion.li
+                            key={tarefa.id}
+                            initial={{ opacity: 0, y: -70 }}
+                            exit={{ opacity: 0, x: 70 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.7 }}
+                        >
+                            {tarefa.titulo}
+                            <button onClick={() => removerTarefa(tarefa.id)}>
+                                <ClearIcon />
+                            </button>
+                        </motion.li>
+                    ))}
+                </AnimatePresence>
             </ul>
         </div>
     );
