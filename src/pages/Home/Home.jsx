f0c5de8c84500b625c54440b5341499d2e0f8444
luiz-000 <luizeduardoferreirasouza@gmail.com>
2025-05-18 22:39:24 -0300

    Importando o componente Dialog

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,9 +6,12 @@ import style from "./Home.module.css";
 import Title from "../../components/Title/Title";
 import Inputs from "../../components/Inputs/Inputs";
 import Tarefas from "../../components/Tarefas/Tarefas";
+import DialogDetalhes from "../../components/DialogDetalhes/DialogDetalhes";
 
 const Home = () => {
     const [tarefas, setTarefas] = useState([]);
+    const [detalhesTarefa, setDetalhesTarefa] = useState(null);
+    const [visibleDialog, setVisibleDialog] = useState(false);
 
     function salvarTarefa(titulo, descricao) {
         const novaTarefa = {
@@ -28,11 +31,28 @@ const Home = () => {
         setTarefas(tarefasRestantes);
     }
 
+    function mostrarDetalhesTarefa(id) {
+        const buscaTarefa = tarefas.find((tarefa) => tarefa.id === id);
+
+        setDetalhesTarefa(buscaTarefa);
+        setVisibleDialog(true);
+    }
+
     return (
         <div className={style.container}>
             <Title />
             <Inputs salvarTarefa={salvarTarefa} />
-            <Tarefas tarefas={tarefas} removerTarefa={removerTarefa} />
+            <Tarefas
+                tarefas={tarefas}
+                removerTarefa={removerTarefa}
+                mostrarDetalhesTarefa={mostrarDetalhesTarefa}
+            />
+
+            <DialogDetalhes
+                open={visibleDialog}
+                onClose={() => setVisibleDialog(false)}
+                detalhesTarefa={detalhesTarefa}
+            />
         </div>
     );
 };
