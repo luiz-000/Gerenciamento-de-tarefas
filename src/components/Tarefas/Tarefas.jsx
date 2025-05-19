3553ea427c3bbef1ed31720a727be325e33810b0
luiz-000 <luizeduardoferreirasouza@gmail.com>
2025-05-18 22:41:48 -0300

    Ajustando a quantidade de caracteres do título de cada tarefa que ficam visíveis na tela principal

--- src/components/Tarefas/Tarefas.jsx
+++ src/components/Tarefas/Tarefas.jsx
@@ -1,10 +1,17 @@
 import { motion, AnimatePresence } from "framer-motion";
 
 import ClearIcon from "@mui/icons-material/Clear";
+import DensityMediumIcon from "@mui/icons-material/DensityMedium";
 
 import style from "./Tarefas.module.css";
 
-const Tarefas = ({ tarefas, removerTarefa }) => {
+const MAX_LENGTH = 28; // número máximo de caracteres visíveis
+
+function cortarTexto(texto, limite) {
+    return texto.length > limite ? texto.substring(0, limite) + "..." : texto;
+}
+
+const Tarefas = ({ tarefas, removerTarefa, mostrarDetalhesTarefa }) => {
     return (
         <div className={style.container}>
             <ul>
@@ -17,10 +24,25 @@ const Tarefas = ({ tarefas, removerTarefa }) => {
                             animate={{ opacity: 1, y: 0 }}
                             transition={{ duration: 0.7 }}
                         >
-                            {tarefa.titulo}
-                            <button onClick={() => removerTarefa(tarefa.id)}>
-                                <ClearIcon />
-                            </button>
+                            {cortarTexto(tarefa.titulo, MAX_LENGTH)}
+
+                            <div className={style.buttonsAction}>
+                                <button
+                                    className={style.buttonDetail}
+                                    onClick={() =>
+                                        mostrarDetalhesTarefa(tarefa.id)
+                                    }
+                                >
+                                    <DensityMediumIcon fontSize="x-small" />
+                                </button>
+
+                                <button
+                                    className={style.buttonDelete}
+                                    onClick={() => removerTarefa(tarefa.id)}
+                                >
+                                    <ClearIcon fontSize="small" />
+                                </button>
+                            </div>
                         </motion.li>
                     ))}
                 </AnimatePresence>
